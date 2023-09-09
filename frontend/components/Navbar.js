6d2a8c7e18b0de0b2185268dacc1a1b1ec4fad78
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-09-08 20:36:25 -0400

    update

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -2,27 +2,27 @@
 import React, {useEffect, useState} from 'react'
 import { FaShoppingCart } from 'react-icons/fa'; // Import the cart icon
 
-const Navbar = ({ cartItems, onLogout }) => {
+const Navbar = ({ cartItems, logout }) => {
     const [userEmail, setUserEmail] = useState('');
     useEffect(() => {
         const email = localStorage.getItem('email');
         setUserEmail(email);
       }, []);
     return (
-        <nav>
+        <nav className='flex p-8'>
         <div className="navbar-left">
           <h1>Your E-Commerce Store</h1>
         </div>
         <div className="navbar-right">
           {userEmail ? (
-            <div className="user-info">
+            <div className="flex">
               <span>Hello, {userEmail.username}</span>
               <span>Cart: {cartItems}</span>
               <FaShoppingCart />
-              <button onClick={onLogout}>Logout</button>
+              <button onClick={logout}>Logout</button>
             </div>
           ) : (
-            <div className="auth-links">
+            <div className="flex">
               <a href="/login">Login</a>
               <a href="/register">Register</a>
             </div>
